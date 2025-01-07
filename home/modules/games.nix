fe0ae422e1a957f80496dabcbdd22dbc4c10f50c
itscrystalline <pvpthadgaming@gmail.com>
2025-01-07 18:10:52 +0700

    gamering

---
+++ home/modules/games.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    (prismlauncher.override {
+      # Add binary required by some mod
+      additionalPrograms = [ ffmpeg ];
+
+      gamemodeSupport = true;
+
+      # Change Java runtimes available to Prism Launcher
+      jdks = [
+        graalvm-ce
+      ];
+    })
+
+    gamemode
+    # Proton
+    protonup-qt
+  ];
+
+  # programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  };
+}
