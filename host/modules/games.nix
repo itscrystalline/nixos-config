47530891e61d5efb82210792aa4cb5fdaddc207c
itscrystalline <pvpthadgaming@gmail.com>
2025-01-07 19:36:26 +0700

    gamer compat

--- host/modules/games.nix
+++ host/modules/games.nix
@@ -10,4 +10,38 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   };
+
+  # Compat
+  programs.nix-ld.enable = true;
+
+  # "minimum" amount of libraries needed for most games to run without steam-run
+  programs.nix-ld.libraries = with pkgs; [
+    # common requirement for several games
+    stdenv.cc.cc.lib
+
+    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L72-L79
+    xorg.libXcomposite
+    xorg.libXtst
+    xorg.libXrandr
+    xorg.libXext
+    xorg.libX11
+    xorg.libXfixes
+    libGL
+    libva
+
+    # from https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/games/steam/fhsenv.nix#L124-L136
+    fontconfig
+    freetype
+    xorg.libXt
+    xorg.libXmu
+    libogg
+    libvorbis
+    SDL
+    SDL2_image
+    glew110
+    libdrm
+    libidn
+    tbb
+    zlib
+  ];
 }
