b6cb7d97a171d174b03063c9737215f2591acc4a
itscrystalline <pvpthadgaming@gmail.com>
2025-01-08 08:41:43 +0700

    change blahaj theme impl

--- host/modules/boot.nix
+++ host/modules/boot.nix
@@ -4,7 +4,26 @@
     #plymouth
     plymouth = {
       enable = true;
-      themePackages = [ (import ./plymouth-blahaj-theme.nix { inherit pkgs; }) ];
+      themePackages = [ (pkgs.plymouth-blahaj-theme.overrideAttrs (old: {
+        patchPhase = ''
+            runHook prePatch
+
+            shopt -s extglob
+
+            # deal with all the non ascii stuff
+            mv !(*([[:graph:]])) blahaj.plymouth
+            sed -i 's/\xc3\xa5/a/g' blahaj.plymouth
+            sed -i 's/\xc3\x85/A/g' blahaj.plymouth
+
+            # colors!
+            sed -i 's/0x000000/0x11111b/g' blahaj.plymouth
+
+            # watermark
+            cp ${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png watermark.png
+
+            runHook postPatch
+        '';
+      })) ];
       theme = "blahaj";
     };
 
