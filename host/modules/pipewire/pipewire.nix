8e0b31bf4f537feeba2822ccbe742b7091c0065d
itscrystalline <pvpthadgaming@gmail.com>
2025-01-08 12:21:42 +0700

    bluetooth audio 2

--- host/modules/pipewire/pipewire.nix
+++ host/modules/pipewire/pipewire.nix
@@ -54,7 +54,8 @@
           "bluez5.enable-sbc-xq" = true;
           "bluez5.enable-msbc" = true;
           "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" "a2dp_sink" ];
+          "bluez5.roles" = [ "a2dp_sink" "a2dp_source" "bap_sink" "bap_source" "hfp_hf" "hfp_ag" ];
+          "bluez5.codecs" = [ "sbc" "sbc_xq" "msbc" "aac" "aptx" "aptx_hd" "ldac" ];
       };
     };
 
