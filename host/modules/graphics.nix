3523ae87b93ba6b52c45fab8c8acceb8c0e6c2ee
itscrystalline <pvpthadgaming@gmail.com>
2025-01-08 22:29:08 +0700

    recentralize cachix

--- host/modules/graphics.nix
+++ host/modules/graphics.nix
@@ -22,16 +22,8 @@
     };
   };
 
-  # CUDA & Cachix
+  # CUDA
   environment.systemPackages = with pkgs; [
-    cudatoolkit
+    unstable.cudatoolkit
   ];
-  nix.settings = {
-    substituters = [
-      "https://cuda-maintainers.cachix.org"
-    ];
-    trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    ];
-  };
 }
--- host/modules/hyprland.nix
+++ host/modules/hyprland.nix
@@ -1,15 +1,5 @@
 { config, pkgs, ... }@inputs:
 {
-  # Cachix
-  nix.settings = {
-    substituters = [
-      "https://hyprland.cachix.org"
-    ];
-    trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
-  };
-
   programs.hyprland = {
     enable = true;
     # set the flake package
