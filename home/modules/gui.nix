523b4094f9fdf84d72b117365900878356f34937
itscrystalline <pvpthadgaming@gmail.com>
2025-03-25 12:32:38 +0700

    added mime default apps

--- home/modules/gui.nix
+++ home/modules/gui.nix
@@ -88,17 +88,69 @@
     };
   };
 
-  # Default Browser
+  # Default Apps
   xdg.mimeApps = pkgs.lib.mkIf config.gui {
     enable = true;
-    defaultApplications = (let browser = "zen.desktop"; in {
+    defaultApplications = (
+      let 
+        browser = "zen.desktop";
+        image_viewer = "org.gnome.Loupe.desktop";
+        pdf_viewer = "org.gnome.Evince.desktop";
+        text_editor = "org.gnome.TextEditor.desktop";
+        video_player = "org.gnome.Totem.desktop";
+        archiver = "org.gnome.FileRoller.desktop";
+      in {
+        # Browser-related MIME types
         "text/html" = browser;
         "text/xml" = browser;
         "application/xhtml+xml" = browser;
         "application/vnd.mozilla.xul+xml" = browser;
         "x-scheme-handler/http" = browser;
         "x-scheme-handler/https" = browser;
-    });
+
+        # Image MIME types
+        "image/jpeg" = image_viewer;
+        "image/png" = image_viewer;
+        "image/gif" = image_viewer;
+        "image/svg+xml" = image_viewer;
+        "image/webp" = image_viewer;
+        "image/bmp" = image_viewer;
+        "image/tiff" = image_viewer;
+        "image/x-icon" = image_viewer;
+        "image/vnd.microsoft.icon" = image_viewer;
+        "image/heif" = image_viewer;
+        "image/heic" = image_viewer;
+        "image/avif" = image_viewer;
+
+        # PDF MIME types
+        "application/pdf" = pdf_viewer;
+
+        # Text file MIME types
+        "text/plain" = text_editor;
+        "text/css" = text_editor;
+        "text/csv" = text_editor;
+        "text/markdown" = text_editor;
+        "application/json" = text_editor;
+        "application/xml" = text_editor;
+
+        # Video MIME types
+        "video/mp4" = video_player;
+        "video/webm" = video_player;
+        "video/x-msvideo" = video_player;  # AVI
+        "video/quicktime" = video_player;  # MOV
+        "video/ogg" = video_player;
+        "video/mpeg" = video_player;
+        "video/3gpp" = video_player;
+        "video/3gpp2" = video_player;
+        "video/x-matroska" = video_player; # MKV
+
+        # Archive MIME types (optional)
+        "application/zip" = archiver;
+        "application/gzip" = archiver;
+        "application/vnd.rar" = archiver;
+        "application/x-7z-compressed" = archiver; 
+      }
+    );
   };
   home.sessionVariables.DEFAULT_BROWSER = pkgs.lib.optionalString config.gui "${zen-browser.packages.${pkgs.system}.default}/bin/zen";
 
