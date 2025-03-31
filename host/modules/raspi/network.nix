dd31c0d9d1cd63fd3630ef9e189297cb7c5a9fec
itscrystalline <pvpthadgaming@gmail.com>
2025-03-31 09:21:14 +0700

    tuned server down a bit & added collabora

--- host/modules/raspi/network.nix
+++ host/modules/raspi/network.nix
@@ -14,4 +14,10 @@
     enable = true;
     allowedTCPPorts = [80 443 8080];
   };
+
+  # nextcloud - collabora loopback
+  networking.hosts = {
+    "127.0.0.1" = ["${config.services.nextcloud.hostName}" "${config.services.collabora-online.settings.server_name}"];
+    "::1" = ["${config.services.nextcloud.hostName}" "${config.services.collabora-online.settings.server_name}"];
+  };
 }
--- host/modules/raspi/services.nix
+++ host/modules/raspi/services.nix
@@ -80,14 +80,20 @@
     # https://spot13.com/pmcalculator/
     poolSettings = {
       pm = "dynamic";
-      "pm.max_children" = "115";
-      "pm.max_spare_servers" = "86";
-      "pm.min_spare_servers" = "28";
-      "pm.start_servers" = "28";
+      "pm.max_children" = "75";
+      "pm.max_spare_servers" = "50";
+      "pm.min_spare_servers" = "25";
+      "pm.start_servers" = "25";
+      "pm.max_requests" = "750";
+    };
+    phpOptions = {
+      "opcache.enabled" = "true";
+      "opcache.jit" = "on";
+      "opcache.jit_buffer_size" = "128M";
     };
 
     extraApps = with config.services.nextcloud.package.packages.apps; {
-      inherit news contacts calendar tasks recognize phonetrack memories previewgenerator notes groupfolders;
+      inherit news contacts calendar tasks recognize phonetrack memories previewgenerator notes groupfolders richdocuments;
     };
     extraAppsEnable = true;
 
@@ -131,8 +137,77 @@
       return-size = true;
     };
   };
-  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-    forceSSL = true;
-    enableACME = true;
+  services.collabora-online = {
+    enable = true;
+    port = 9980; # default
+    settings = {
+      # Rely on reverse proxy for SSL
+      ssl = {
+        enable = false;
+        termination = true;
+      };
+
+      # Listen on loopback interface only, and accept requests from ::1
+      net = {
+        listen = "loopback";
+        post_allow.host = ["::1"];
+      };
+
+      # Restrict loading documents from WOPI Host nextcloud.example.com
+      storage.wopi = {
+        "@allow" = true;
+        host = ["nc.iw2tryhard.dev"];
+      };
+
+      # Set FQDN of server
+      server_name = "collabora.iw2tryhard.dev";
+    };
+  };
+  services.nginx = {
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts.${config.services.nextcloud.hostName} = {
+      forceSSL = true;
+      enableACME = true;
+    };
+    virtualHosts.${config.services.collabora-online.settings.server_name} = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://[::1]:${toString config.services.collabora-online.port}";
+        proxyWebsockets = true; # collabora uses websockets
+      };
+    };
+  };
+
+  # SSH auto restart
+  systemd.services.sshd.serviceConfig.Restart = lib.mkForce "always";
+  systemd.services.tailscaled.serviceConfig.Restart = lib.mkForce "always";
+
+  # collabora setup; https://diogotc.com/blog/collabora-nextcloud-nixos/
+  systemd.services.nextcloud-config-collabora = let
+    inherit (config.services.nextcloud) occ;
+
+    wopi_url = "http://[::1]:${toString config.services.collabora-online.port}";
+    public_wopi_url = "https://collabora.example.com";
+    wopi_allowlist = lib.concatStringsSep "," [
+      "127.0.0.1"
+      "::1"
+    ];
+  in {
+    wantedBy = ["multi-user.target"];
+    after = ["nextcloud-setup.service" "coolwsd.service"];
+    requires = ["coolwsd.service"];
+    script = ''
+      ${occ}/bin/nextcloud-occ config:app:set richdocuments wopi_url --value ${lib.escapeShellArg wopi_url}
+      ${occ}/bin/nextcloud-occ config:app:set richdocuments public_wopi_url --value ${lib.escapeShellArg public_wopi_url}
+      ${occ}/bin/nextcloud-occ config:app:set richdocuments wopi_allowlist --value ${lib.escapeShellArg wopi_allowlist}
+      ${occ}/bin/nextcloud-occ richdocuments:setup
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "nextcloud";
+    };
   };
 }
