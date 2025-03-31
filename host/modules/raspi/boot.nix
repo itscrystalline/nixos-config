f1c397d510cb0e81154b14caf2f39be560cc12fc
itscrystalline <pvpthadgaming@gmail.com>
2025-03-31 09:20:58 +0700

    added initrd ssh

--- host/modules/raspi/boot.nix
+++ host/modules/raspi/boot.nix
@@ -1,7 +1,15 @@
 {pkgs, ...}: {
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
+    initrd = {
+      availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
+      network.ssh = {
+        enable = true;
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPS4b7HxJiG6gAOvqw/fD5CKWP3HqOFdfi2zpwmPi4wu itscrystalline@cwystaws-meowchine"
+        ];
+      };
+    };
     loader = {
       grub.enable = false;
       generic-extlinux-compatible.enable = true;
