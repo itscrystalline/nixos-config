016ac0151472793a83bfde3ff1da780fba9612a7
itscrystalline <pvpthadgaming@gmail.com>
2025-04-04 17:07:37 +0700

    update site config

--- host/modules/raspi/services/iw2tryhard-dev.nix
+++ host/modules/raspi/services/iw2tryhard-dev.nix
@@ -4,8 +4,10 @@
   ...
 }: let
   site = inputs.iw2tryhard-dev.packages.${pkgs.system}.docker;
+  # site_v2 = inputs.iw2tryhard-dev.packages.${pkgs.system}.docker_v2;
   domain = "iw2tryhard.dev";
   port = "3000";
+  port_v2 = "3001";
 in {
   virtualisation.oci-containers = {
     backend = "docker";
@@ -15,13 +17,28 @@ in {
         imageFile = site;
         ports = ["127.0.0.1:${port}:3000"];
         extraOptions = ["--network=bridge" "--dns=1.1.1.1"];
+        autoStart = true;
+      };
+      thaddev-com = {
+        image = "ghcr.io/itscrystalline/thaddev.com-2.0:main";
+        # imageFile = site_v2;
+        ports = ["127.0.0.1:${port_v2}:3000"];
+        extraOptions = ["--network=bridge" "--dns=1.1.1.1"];
+        autoStart = true;
       };
     };
   };
-  services.nginx.virtualHosts.${domain} = {
-    serverAliases = ["www.${domain}"];
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${port}";
+  services.nginx.virtualHosts = {
+    ${domain} = {
+      serverAliases = ["www.${domain}"];
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${port}";
+      };
+    };
+    "v2.${domain}" = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:${port_v2}";
+      };
     };
   };
 }
