f4e3f7bd3ce57913bca92a7479ece8880b870200
itscrystalline <pvpthadgaming@gmail.com>
2025-04-07 12:15:44 +0700

    clean up config a bit

--- host/modules/raspi/services/grafana.nix
+++ host/modules/raspi/services/grafana.nix
@@ -1,7 +1,6 @@
 {
   config,
   mkLocalNginx,
-  ...
 }: {
   imports = [
     (mkLocalNginx "grafana" config.services.grafana.settings.server.http_port true)
@@ -178,80 +177,5 @@
       ];
     };
     # extraFlags
-  };
-
-  # nginx reverse proxy
-  services.nginx = {
-    # upstreams = {
-    #   "grafana" = {
-    #     servers = {
-    #       "127.0.0.1:${toString config.services.grafana.settings.server.http_port}" = {};
-    #     };
-    #   };
-    #   "prometheus" = {
-    #     servers = {
-    #       "127.0.0.1:${toString config.services.prometheus.port}" = {};
-    #     };
-    #   };
-    #   "loki" = {
-    #     servers = {
-    #       "127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}" = {};
-    #     };
-    #   };
-    #   "promtail" = {
-    #     servers = {
-    #       "127.0.0.1:${toString config.services.promtail.configuration.server.http_listen_port}" = {};
-    #     };
-    #   };
-    # };
-    #
-    # virtualHosts.grafana = {
-    #   locations."/" = {
-    #     proxyPass = "http://grafana";
-    #     proxyWebsockets = true;
-    #   };
-    #   serverName = "grafana.crys";
-    #   # listen = [
-    #   #   {
-    #   #     addr = "192.168.1.10";
-    #   #     port = 8010;
-    #   #   }
-    #   # ];
-    # };
-    #
-    # virtualHosts.prometheus = {
-    #   locations."/".proxyPass = "http://prometheus";
-    #   serverName = "prometheus.crys";
-    #   # listen = [
-    #   #   {
-    #   #     addr = "192.168.1.10";
-    #   #     port = 8020;
-    #   #   }
-    #   # ];
-    # };
-    #
-    # # confirm with http://192.168.1.10:8030/loki/api/v1/status/buildinfo
-    # #     (or)     /config /metrics /ready
-    # virtualHosts.loki = {
-    #   locations."/".proxyPass = "http://loki";
-    #   serverName = "loki.crys";
-    #   # listen = [
-    #   #   {
-    #   #     addr = "192.168.1.10";
-    #   #     port = 8030;
-    #   #   }
-    #   # ];
-    # };
-    #
-    # virtualHosts.promtail = {
-    #   locations."/".proxyPass = "http://promtail";
-    #   serverName = "promtail.crys";
-    #   # listen = [
-    #   #   {
-    #   #     addr = "192.168.1.10";
-    #   #     port = 8031;
-    #   #   }
-    #   # ];
-    # };
   };
 }
