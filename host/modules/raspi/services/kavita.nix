0888f039c97094bdeeb89c7cfea9576803af9567
itscrystalline <pvpthadgaming@gmail.com>
2025-04-21 20:49:47 +0700

    change komf kavita path

--- host/modules/raspi/services/kavita.nix
+++ host/modules/raspi/services/kavita.nix
@@ -19,10 +19,10 @@
       image = "sndxr/komf:latest";
       ports = ["127.0.0.1::8085"];
       user = "1000:1000";
-      extraOptions = ["--network=bridge" "--dns=1.1.1.1"];
+      extraOptions = ["--network=bridge" "--dns=100.100.100.100"];
       autoStart = true;
       environment = {
-        KOMF_KAVITA_BASE_URI = "http://127.0.0.1:${builtins.toString config.services.kavita.settings.Port}";
+        KOMF_KAVITA_BASE_URI = "http://manga.crys";
         KOMF_KAVITA_API_KEY = secrets.kavita_api_key;
         KOMF_LOG_LEVEL = "INFO";
         JAVA_TOOL_OPTIONS = "-XX:+UnlockExperimentalVMOptions -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=compact -XX:ShenandoahGuaranteedGCInterval=3600000 -XX:TrimNativeHeapInterval=3600000";
