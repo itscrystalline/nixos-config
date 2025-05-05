4bcff748646c116f80383102e68bb7b8c296e6a5
itscrystalline <pvpthadgaming@gmail.com>
2025-05-05 20:09:52 +0700

    update sipa and occasion

--- host/modules/common/thai-sarabun-font.nix
+++ host/modules/common/thai-sarabun-font.nix
@@ -5,7 +5,8 @@ pkgs.stdenvNoCC.mkDerivation {
 
   src = pkgs.fetchzip {
     url = "https://oer.learn.in.th/search_detail/ZipDownload/220410";
-    sha256 = "sha256-oVNhnxskjUO6QUUzeYTAm4yjdbpjGdjNeApVAt9bcg0=";
+    sha256 = "sha256-F1BWYueWPXESgKsuMIq0gZlJRDjQOJKcELatjWKJrYw=";
+    extension = "zip";
     stripRoot = false;
   };
 
