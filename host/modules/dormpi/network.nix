b67d9f7ceeca6899da46ca13323904d121aef840
itscrystalline <pvpthadgaming@gmail.com>
2025-05-05 15:45:53 +0700

    add wifi hotspot

--- host/modules/dormpi/network.nix
+++ host/modules/dormpi/network.nix
@@ -10,9 +10,40 @@
   networking.hostName = "cwystaws-raspi";
   networking.networkmanager.wifi.powersave = false;
 
-  # nextcloud - collabora loopback
-  # networking.hosts = {
-  #   "127.0.0.1" = ["${config.services.nextcloud.hostName}" "${config.services.collabora-online.settings.server_name}"];
-  #   "::1" = ["${config.services.nextcloud.hostName}" "${config.services.collabora-online.settings.server_name}"];
-  # };
+  # "wlan0" is the hardware device, "wlan-station0" is for wifi-client managed by network manager, "wlan-ap0" is for hostap
+  networking.wlanInterfaces = {
+    "wlan-station0" = {device = "wlan0";};
+    "wlan-ap0" = {
+      device = "wlan0";
+      mac = "08:11:96:0e:08:0a";
+    };
+  };
+
+  networking.networkmanager.unmanaged = ["interface-name:wlp*" "interface-name:${config.services.hostapd.interface}"];
+
+  services.hostapd = {
+    enable = true;
+    interface = "wlan-ap0";
+    hwMode = "g";
+    ssid = "dormpi";
+    wpaPassphrase = "crystals_iot_wifi";
+  };
+
+  networking.interfaces."wlan-ap0".ipv4.addresses = [
+    {
+      address = "192.168.12.1";
+      prefixLength = 24;
+    }
+  ];
+
+  services.dnsmasq = {
+    enable = true;
+    extraConfig = ''
+      interface=wlan-ap0
+      bind-interfaces
+      dhcp-range=192.168.12.10,192.168.12.254,24h
+    '';
+  };
+  networking.firewall.allowedUDPPorts = [53 67]; # DNS & DHCP
+  services.haveged.enable = config.services.hostapd.enable;
 }
