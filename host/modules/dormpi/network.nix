93195907bef5bc6ccb1f0cfcae760f0a4929a660
itscrystalline <pvpthadgaming@gmail.com>
2025-05-06 17:25:56 +0700

    wifi 13

--- host/modules/dormpi/network.nix
+++ host/modules/dormpi/network.nix
@@ -20,35 +20,35 @@
     };
   };
   #
-  networking.networkmanager.unmanaged = ["interface-name:wlp*" "interface-name:wlan-ap0"];
+  networking.networkmanager.unmanaged = ["interface-name:wlp*"];
   #
-  services.hostapd = {
-    enable = true;
-    radios.wlan-ap0 = {
-      countryCode = "TH";
-      channel = 11;
-      settings = {
-        ieee80211n = true;
-        wmm_enabled = true;
-      };
-      networks.wlan-ap0 = {
-        ssid = "dormpi";
-        authentication = {
-          mode = "wpa2-sha256";
-          # saePasswords = [{password = secrets.homeassistant.wifi-password;}];
-          wpaPassword = secrets.homeassistant.wifi-password;
-        };
-      };
-    };
-  };
+  # services.hostapd = {
+  #   enable = true;
+  #   radios.wlan-ap0 = {
+  #     countryCode = "TH";
+  #     channel = 11;
+  #     settings = {
+  #       ieee80211n = true;
+  #       wmm_enabled = true;
+  #     };
+  #     networks.wlan-ap0 = {
+  #       ssid = "dormpi";
+  #       authentication = {
+  #         mode = "wpa2-sha256";
+  #         # saePasswords = [{password = secrets.homeassistant.wifi-password;}];
+  #         wpaPassword = secrets.homeassistant.wifi-password;
+  #       };
+  #     };
+  #   };
+  # };
 
-  networking.interfaces."wlan-ap0".ipv4.addresses = [
-    {
-      address = "192.168.12.1";
-      prefixLength = 24;
-    }
-  ];
+  # networking.interfaces."wlan-ap0".ipv4.addresses = [
+  #   {
+  #     address = "192.168.12.1";
+  #     prefixLength = 24;
+  #   }
+  # ];
 
   networking.firewall.allowedUDPPorts = [53 67]; # DNS & DHCP
-  services.haveged.enable = config.services.hostapd.enable;
+  # services.haveged.enable = config.services.hostapd.enable;
 }
