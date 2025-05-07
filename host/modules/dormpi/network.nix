d0fcc2516fbdba0768cd75dab6d9fbcfd8ccdb67
itscrystalline <pvpthadgaming@gmail.com>
2025-05-07 19:59:51 +0700

    new wifi

--- host/modules/dormpi/network.nix
+++ host/modules/dormpi/network.nix
@@ -12,43 +12,43 @@
   networking.networkmanager.wifi.powersave = false;
 
   # "wlan0" is the hardware device, "wlan-station0" is for wifi-client managed by network manager, "wlan-ap0" is for hostap
-  networking.wlanInterfaces = {
-    "wlan-station0" = {device = "wlan0";};
-    "wlan-ap0" = {
-      device = "wlan0";
-      mac = "08:11:96:0e:08:0a";
-    };
-  };
-  #
-  # networking.networkmanager.unmanaged = ["interface-name:wlp*"];
-  #
-  # services.hostapd = {
-  #   enable = true;
-  #   radios.wlan-ap0 = {
-  #     countryCode = "TH";
-  #     channel = 11;
-  #     settings = {
-  #       ieee80211n = true;
-  #       wmm_enabled = true;
-  #     };
-  #     networks.wlan-ap0 = {
-  #       ssid = "dormpi";
-  #       authentication = {
-  #         mode = "wpa2-sha256";
-  #         # saePasswords = [{password = secrets.homeassistant.wifi-password;}];
-  #         wpaPassword = secrets.homeassistant.wifi-password;
-  #       };
-  #     };
+  # networking.wlanInterfaces = {
+  #   "wlan-station0" = {device = "wlan0";};
+  #   "wlan-ap0" = {
+  #     device = "wlan0";
+  #     mac = "08:11:96:0e:08:0a";
   #   };
   # };
+  #
+  networking.networkmanager.unmanaged = ["interface-name:wlp*"];
+  #
+  services.hostapd = {
+    enable = true;
+    radios.wlp1s0u1u1 = {
+      countryCode = "TH";
+      channel = 11;
+      settings = {
+        ieee80211n = true;
+        wmm_enabled = true;
+      };
+      networks.wlp1s0u1u1 = {
+        ssid = "dormpi";
+        authentication = {
+          mode = "wpa2-sha256";
+          # saePasswords = [{password = secrets.homeassistant.wifi-password;}];
+          wpaPassword = secrets.homeassistant.wifi-password;
+        };
+      };
+    };
+  };
 
-  # networking.interfaces."wlan-ap0".ipv4.addresses = [
-  #   {
-  #     address = "192.168.12.1";
-  #     prefixLength = 24;
-  #   }
-  # ];
+  networking.interfaces."wlp1s0u1u1".ipv4.addresses = [
+    {
+      address = "192.168.12.1";
+      prefixLength = 24;
+    }
+  ];
 
   networking.firewall.allowedUDPPorts = [53 67]; # DNS & DHCP
-  # services.haveged.enable = config.services.hostapd.enable;
+  services.haveged.enable = config.services.hostapd.enable;
 }
