489db028910857cab8fcdf5108b744a40da2a5bd
itscrystalline <pvpthadgaming@gmail.com>
2025-11-19 22:20:53 +0700

    uhh 3

--- post-build-hook.sh
+++ post-build-hook.sh
@@ -1,7 +1,5 @@
 set -eu
 set -f # disable globbing
 
-if [[ -n "${OUT_PATHS}" ]]; then
-    echo "Uploading paths" $OUT_PATHS
-    exec nix copy --to "http://cache.crys" $OUT_PATHS
-fi
+echo "Uploading paths" $OUT_PATHS
+exec nix copy --to "http://cache.crys" $OUT_PATHS || true
