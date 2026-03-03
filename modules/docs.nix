d4a673a6c58225a2ede3b3b51c00cb1fde3db308
itscrystalline <pvpthadgaming@gmail.com>
2026-03-03 18:52:28 +0700

    feat(wip): add auto generated docs

--- modules/docs.nix
+++ modules/docs.nix
@@ -16,22 +16,4 @@
     inherit (eval) options;
   };
 in
-  pkgs.stdenv.mkDerivation {
-    src = ./.;
-    name = "docs";
-    nativeBuildInputs = with pkgs; [
-      mkdocs
-    ];
-    buildPhase = ''
-      mkdir -p docs
-      ln -s ${optionsDoc.optionsCommonMark} "./docs/options.md"
-      cat > mkdocs.yml <<EOF
-      site_name: Crystal's Nix Module Options
-      docs_dir: docs
-      EOF
-      mkdocs build
-    '';
-    installPhase = ''
-      mv site $out
-    '';
-  }
+  optionsDoc.optionsCommonMark
