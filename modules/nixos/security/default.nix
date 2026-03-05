587933883f460fed8608dc7f7c8e4f57ee5fdb1b
itscrystalline <pvpthadgaming@gmail.com>
2026-03-05 13:21:25 +0700

    feat: added gnome polkit back

--- modules/nixos/security/default.nix
+++ modules/nixos/security/default.nix
@@ -1,4 +1,9 @@
-{config, pkgs, ...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   environment.systemPackages = with pkgs; [doas-sudo-shim];
 
   security = {
@@ -14,6 +19,20 @@
       ];
     };
     polkit.enable = true;
+  };
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = lib.mkIf config.gui.enable {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = ["graphical-session.target"];
+    wants = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
   };
 
   users.users.${config.core.primaryUser}.extraGroups = ["wheel"];
