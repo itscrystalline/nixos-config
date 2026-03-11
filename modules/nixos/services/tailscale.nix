b1eeff8add5a313b46517736c9c142f26357803b
itscrystalline <pvpthadgaming@gmail.com>
2026-03-11 13:21:55 +0700

    feat: seperate tailscale server and client config

--- modules/nixos/services/tailscale.nix
+++ modules/nixos/services/tailscale.nix
@@ -12,18 +12,24 @@ in {
     role = lib.mkOption {
       type = lib.types.enum ["client" "server"];
       default = "client";
-      description = "The tailscale role this host using. This is to configure required options for tailscale's features to work.";
+      description = "The tailscale role this host using. This is to configure required options for tailscale's features to work. If set to 'server', this also configures the auth key.";
     };
   };
-  config = lib.mkIf enabled {
-    sops.secrets."tailscaleAuthKey".restartUnits = ["tailscaled-autoconnect.service"];
-
-    services.tailscale = {
-      enable = true;
-      useRoutingFeatures = tailscale.role;
-      extraSetFlags = ["--operator=${config.core.primaryUser}" "--ssh"] ++ lib.optional (tailscale.role == "server" && tailscale.asExitNode) "--advertise-exit-node";
-      authKeyFile = config.sops.secrets."tailscaleAuthKey".path;
-    };
-    systemd.services.tailscaled.serviceConfig.Restart = lib.mkForce "always";
-  };
+  config = lib.mkIf enabled (lib.mkMerge [
+    {
+      services.tailscale = {
+        enable = true;
+        useRoutingFeatures = tailscale.role;
+        extraSetFlags = ["--operator=${config.core.primaryUser}"];
+      };
+      systemd.services.tailscaled.serviceConfig.Restart = lib.mkForce "always";
+    }
+    (lib.mkIf (tailscale.role == "server") {
+      sops.secrets."tailscaleAuthKey".restartUnits = ["tailscaled-autoconnect.service"];
+      services.tailscale = {
+        extraSetFlags = ["--ssh"] ++ lib.optional tailscale.asExitNode "--advertise-exit-node";
+        authKeyFile = config.sops.secrets."tailscaleAuthKey".path;
+      };
+    })
+  ]);
 }
