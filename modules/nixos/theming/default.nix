70c2727c4260daaeeec65eea852ecbb8b0aeef6d
itscrystalline <pvpthadgaming@gmail.com>
2026-03-11 21:26:35 +0700

    feat: add background image to limine

--- modules/nixos/theming/default.nix
+++ modules/nixos/theming/default.nix
@@ -5,6 +5,11 @@
   ...
 }: let
   enabled = config.theming.enable;
+
+  boot-wallpaper = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/refs/heads/main/gradients/magenta_pink.png";
+    hash = "sha256-Skj2LLYAPaSZytl8v3sQvsl+fx06CIpbloue11Ti24c=";
+  };
 in {
   imports = [./gui.nix];
 
@@ -15,8 +20,13 @@ in {
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
       polarity = "dark";
       autoEnable = true;
-
-      targets.plymouth.enable = false;
+      targets = {
+        plymouth.enable = false;
+        limine.image = lib.mkIf (config.boot.bootloader == "limine") {
+          enable = true;
+          override = "${boot-wallpaper}";
+        };
+      };
     };
   };
 }
