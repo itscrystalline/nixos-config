8079579162230daba2f0f1232f545327af7ad0be
itscrystalline <pvpthadgaming@gmail.com>
2026-03-20 20:54:44 +0700

    fix: add dependencies to connectivity script

--- hosts/liriel/connectivity-check.nix
+++ hosts/liriel/connectivity-check.nix
@@ -4,6 +4,7 @@
       description = "Check internet connectivity and restart services";
       after = ["network-online.target"];
       wants = ["network-online.target"];
+      path = with pkgs; [util-linux iputils coreutils-full];
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${pkgs.bash}/bin/bash ${./connectivity-check.sh}";
