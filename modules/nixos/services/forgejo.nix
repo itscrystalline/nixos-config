945feed03b86eb7c9f0d445fad3916946bc7950c
itscrystalline <pvpthadgaming@gmail.com>
2026-04-05 09:47:16 +0300

    forgejo: change password part 1

--- modules/nixos/services/forgejo.nix
+++ modules/nixos/services/forgejo.nix
@@ -28,7 +28,8 @@ in {
       pwd = config.sops.secrets.forgejo-admin-password;
       user = "itscrystalline"; # Note, Forgejo doesn't allow creation of an account named "admin"
     in ''
-      ${adminCmd} create --admin --email "real@iw2tryhard.dev" --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
+      # ${adminCmd} create --admin --email "real@iw2tryhard.dev" --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
+      ${adminCmd} change-password --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
     '';
 
     services = {
