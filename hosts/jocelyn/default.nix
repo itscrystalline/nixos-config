01b4138b436c11a1cec330a07b3455a33417b165
itscrystalline <pvpthadgaming@gmail.com>
2026-04-09 17:41:59 +0700

    jocelyn: populate (wip)

--- hosts/jocelyn/default.nix
+++ hosts/jocelyn/default.nix
@@ -1,2 +1,34 @@
 {...}: {
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512MiB";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = ["umask=0077"];
+            };
+          };
+
+          root = {
+            # 30GB SSD minus ESP overhead.
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = ["noatime"];
+            };
+          };
+        };
+      };
+    };
+  };
 }
