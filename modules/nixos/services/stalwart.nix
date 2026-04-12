8901e2f4d4a9ad177095acea684582ac33b4e38d
itscrystalline <pvpthadgaming@gmail.com>
2026-04-12 20:00:43 +0300

    stalwart: revert to nixos module

--- modules/nixos/services/stalwart.nix
+++ modules/nixos/services/stalwart.nix
@@ -5,48 +5,172 @@
 }: let
   inherit (config.crystals-services) stalwart;
   enabled = stalwart.enable;
-
-  p = p: let strp = builtins.toString p; in "${strp}:${strp}";
 in {
   options.crystals-services.stalwart = {
-    enable = lib.mkEnableOption "stalwart (docker)";
+    enable = lib.mkEnableOption "stalwart mail server";
+
+    directory = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/stalwart-mail";
+      description = "Directory to store stalwart data";
+    };
 
     host = lib.mkOption {
-      type = lib.types.str;
-      description = "Mail domain (e.g. example.com).";
-      default = "";
+      type = lib.types.nullOr lib.types.str;
+      description = "Main stalwart hostname.";
+      default = "iw2tryhard.dev";
+    };
+
+    webUIHost = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      description = "Nginx local host name to proxy stalwart web UI under. Null to disable.";
+      default = "stalwart.crys";
     };
   };
 
   config = lib.mkIf enabled {
-    assertions = [
-      {
-        assertion = stalwart.host != "";
-        message = "crystals-services.stalwart.host must be set to a non-empty domain when enabled.";
-      }
-    ];
-
-    systemd.tmpfiles.rules = [
-      "d '/var/lib/stalwart' - root root - -"
-    ];
-
-    nginx.virtualHosts."stalwart.${config.crystals-services.nginx.localSuffix}".locations."/" = {
-      proxyPass = "http://127.0.0.1:8080";
-      proxyWebsockets = true;
+    sops.secrets = {
+      stalwart-admin-password.owner = "stalwart-mail";
+      stalwart-cloudflare-token.owner = "stalwart-mail";
     };
-    virtualisation.oci-containers.containers = {
-      stalwart = {
-        image = "stalwartlabs/stalwart:latest";
-        ports = [
-          (p 25)
-          (p 443)
-          (p 465)
-          (p 993)
-          "127.0.0.1:8080:8080"
+
+    services.stalwart-mail = {
+      enable = true;
+      dataDir = stalwart.directory;
+      openFirewall = true;
+
+      credentials = {
+        admin_pass = config.sops.secrets.stalwart-admin-password.path;
+        cloudflare_token = config.sops.secrets.stalwart-cloudflare-token.path;
+      };
+
+      settings = {
+        server = {
+          hostname = "mx1.${stalwart.host}";
+          max-connections = 8192;
+          socket = {
+            backlog = 1024;
+            nodelay = true;
+            reuse-addr = true;
+            reuse-port = true;
+          };
+          listener = {
+            smtp = {
+              bind = "[::]:25";
+              protocol = "smtp";
+            };
+            submissions = {
+              bind = "[::]:465";
+              protocol = "smtp";
+              tls.implicit = true;
+            };
+            imaptls = {
+              bind = "[::]:993";
+              protocol = "imap";
+              tls.implicit = true;
+            };
+            sieve = {
+              bind = "[::]:4190";
+              protocol = "managesieve";
+            };
+            https = {
+              bind = "[::]:443";
+              protocol = "http";
+              tls.implicit = true;
+            };
+            http = {
+              bind = "[::]:8080";
+              protocol = "http";
+            };
+          };
+        };
+
+        lookup.default = {
+          hostname = "mx1.${stalwart.host}";
+          domain = stalwart.host;
+        };
+
+        acme."letsencrypt" = {
+          directory = "https://acme-v02.api.letsencrypt.org/directory";
+          challenge = "dns-01";
+          contact = "real@${stalwart.host}";
+          domains = [
+            # "${stalwart.host}"
+            "mx1.${stalwart.host}"
+            "mail.${stalwart.host}"
+            "autoconfig.${stalwart.host}"
+            "autodiscover.${stalwart.host}"
+            "mta-sts.${stalwart.host}"
+          ];
+          provider = "cloudflare";
+          secret = "%{file:/run/credentials/stalwart-mail.service/cloudflare_token}%";
+        };
+
+        session = {
+          auth = {
+            mechanisms = "[plain]";
+            directory = "'internal'";
+          };
+          rcpt = {
+            directory = "'internal'";
+            relay = [
+              {
+                "if" = "!is_local_domain('*', rcpt_domain) && authenticated_as != ''";
+                "then" = "true";
+              }
+              {"else" = "false";}
+            ];
+          };
+        };
+
+        imap = {
+          auth = {
+            directory = "'internal'";
+            mechanisms = "[plain]";
+          };
+        };
+
+        storage = {
+          data = "db";
+          fts = "db";
+          blob = "db";
+          lookup = "db";
+          directory = "internal";
+        };
+
+        directory.internal = {
+          type = "internal";
+          store = "db";
+          lookup.domains = ["${stalwart.host}"];
+        };
+
+        authentication.fallback-admin = {
+          user = "admin";
+          secret = "%{file:/run/credentials/stalwart-mail.service/admin_pass}%";
+        };
+
+        queue.strategy.route = [
+          {
+            "if" = "is_local_domain('*', rcpt_domain)";
+            "then" = "'local'";
+          }
+          {"else" = "'mx'";}
         ];
-        volumes = [
-          "/var/lib/stalwart:/opt/stalwart"
-        ];
+
+        tracer.stdout = {
+          type = "stdout";
+          level = "info";
+          ansi = false;
+          enable = true;
+        };
+      };
+    };
+
+    # nginx reverse proxy for web UI
+    services.nginx.virtualHosts = lib.mkIf (stalwart.webUIHost != null) {
+      "${stalwart.webUIHost}".locations."/" = {
+        proxyPass = "http://127.0.0.1:8080";
+        proxyWebsockets = true;
       };
     };
   };
