8923a19256df2a1cdc6c707787184e353f99d7c0
itscrystalline <pvpthadgaming@gmail.com>
2026-05-01 11:04:48 +0300

    forgejo: add different runner labels depending on arch

--- modules/nixos/services/forgejo.nix
+++ modules/nixos/services/forgejo.nix
@@ -151,22 +151,24 @@ in {
           # Obtaining the path to the runner token file may differ
           # tokenFile should be in format TOKEN=<secret>, since it's EnvironmentFile for systemd
           tokenFile = config.sops.secrets.forgejo-runner-token.path;
-          labels = [
-            "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:runner-latest"
-            "ubuntu-24.04:docker://ghcr.io/catthehacker/ubuntu:runner-24.04"
-            "ubuntu-22.04:docker://ghcr.io/catthehacker/ubuntu:runner-22.04"
-            "ubuntu-20.04:docker://ghcr.io/catthehacker/ubuntu:runner-20.04"
-            "ubuntu-18.04:docker://ghcr.io/catthehacker/ubuntu:runner-18.04"
-
-            "ubuntu-latest-arm:docker://ghcr.io/catthehacker/ubuntu:runner-latest"
-            "ubuntu-24.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-24.04"
-            "ubuntu-22.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-22.04"
-            "ubuntu-20.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-20.04"
-            "ubuntu-18.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-18.04"
-
-            "node:docker://node:16-bullseye"
-            "native:host"
-          ];
+          labels =
+            (lib.optionals pkgs.stdenv.hostPlatform.isx86_64 [
+              "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:runner-latest"
+              "ubuntu-24.04:docker://ghcr.io/catthehacker/ubuntu:runner-24.04"
+              "ubuntu-22.04:docker://ghcr.io/catthehacker/ubuntu:runner-22.04"
+              "ubuntu-20.04:docker://ghcr.io/catthehacker/ubuntu:runner-20.04"
+              "ubuntu-18.04:docker://ghcr.io/catthehacker/ubuntu:runner-18.04"
+            ])
+            ++ (lib.optionals pkgs.stdenv.hostPlatform.isAarch64 [
+              "ubuntu-latest-arm:docker://ghcr.io/catthehacker/ubuntu:runner-latest"
+              "ubuntu-24.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-24.04"
+              "ubuntu-22.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-22.04"
+              "ubuntu-20.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-20.04"
+              "ubuntu-18.04-arm:docker://ghcr.io/catthehacker/ubuntu:runner-18.04"
+            ])
+            ++ [
+              "native:host"
+            ];
         };
       };
     })
