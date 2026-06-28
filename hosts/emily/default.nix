d9466451410a8386fb0e2d1edc142cb82d9b9179
itscrystalline <crystal@iw2tryhard.dev>
2026-06-28 13:43:25 +0700

    emily: bump zram, add swap

--- hosts/emily/default.nix
+++ hosts/emily/default.nix
@@ -1,4 +1,13 @@
 _: {
   imports = [./disko.nix ./facter.nix];
   services.logind.settings.Login.HandleLidSwitch = "ignore";
+
+  swapDevices = [
+    {
+      device = "/var/swap";
+      size = 4096;
+    }
+  ];
+
+  zramSwap.memoryPercent = 100;
 }
