b0bdcf8817fb0ca66d54662bf58931b02e32365a
itscrystalline <crystal@iw2tryhard.dev>
2026-07-03 14:47:16 +0700

    liriel: add nmcle to conn-check

--- hosts/liriel/connectivity-check.nix
+++ hosts/liriel/connectivity-check.nix
@@ -4,7 +4,7 @@
       description = "Check internet connectivity and restart services";
       after = ["network-online.target"];
       wants = ["network-online.target"];
-      path = with pkgs; [util-linux iputils coreutils-full];
+      path = with pkgs; [util-linux iputils coreutils-full networkmanager];
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${pkgs.bash}/bin/bash ${./connectivity-check.sh}";
